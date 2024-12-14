5cd8fac9a3f5b12272a865746f4e8096a89f66d4
JAMES M <168496401+princelamar509@users.noreply.github.com>
2024-12-13 23:19:53 -0500

    update

--- online-shopping/src/pages/ProductDetailPage.jsx
+++ online-shopping/src/pages/ProductDetailPage.jsx
@@ -5,6 +5,9 @@ import '../styles/App.css';
 const ProductDetailPage = ({ addToCart }) => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  
+
+
 
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products/${id}`)
@@ -19,12 +22,12 @@ const ProductDetailPage = ({ addToCart }) => {
 
   const handleBuyNow = () => {
     addToCart(product);
-    window.location.href = '/checkout';
+    window.location.href ='/Checkout';
   };
 
   const handleAddToWishlist = () => {
     addToCart(product);
-    window.location.href = '/wishlist';
+    window.location.href ='/wishlist';
   };
 
   return (
@@ -33,11 +36,9 @@ const ProductDetailPage = ({ addToCart }) => {
       <h1>{product.title}</h1>
       <p>{product.description}</p>
       <h2>${product.price.toFixed(2)}</h2>
-      <button onClick={() => addToCart(product)}>Add to Cart</button>
-      <button onClick={handleBuyNow}>Buy Now</button>
-      <button onClick={handleAddToWishlist}>Add to Wishlist</button>
-
-      
+      <button className='detailbtn' onClick={() => addToCart(product)}>Add to Cart</button>
+      <button className='detailbtn'  onClick={handleBuyNow}>Buy Now</button>
+      <button className='detailbtn' onClick={handleAddToWishlist}>Add to Wishlist</button>
     </div>
 
 
