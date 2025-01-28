92e44f348ba08e274517a441f57991fae35f6acf
JAMES M <168496401+princelamar509@users.noreply.github.com>
2025-01-28 17:54:44 -0500

    g

--- online-shopping/src/pages/ContactPage.jsx
+++ online-shopping/src/pages/ContactPage.jsx
@@ -9,14 +9,14 @@ const ContactPage = () => {
       <div className="contact-page">
         <h2>Contact Us</h2>
         <form>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" required />
+          <label htmlFor="name" >Name</label>
+          <input type="text" id="name" name="name" required placeholder='Enter your name'/>
 
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" required />
+          <label htmlFor="email" >Email</label>
+          <input type="email" id="email" name="email" required placeholder='Enter your email'/>
 
           <label htmlFor="message">Message</label>
-          <textarea id="message" name="message" required></textarea>
+          <textarea id="message" name="message" required placeholder='Enter your message'></textarea>
 
           <button type="submit">Send Message</button>
         </form>
